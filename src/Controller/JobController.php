e3a564c81167d564632113ef975ae37afe374dc6
Eduard Climov <klimovedik88@gmail.com>
2018-05-01 18:52:37 +0300

    Namespace updated, according to best practices

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Job;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
