9d1ab4475dd9ad2030dfc064151a1a293f7f4c76
Constantin <kostelu@gmail.com>
2018-05-07 21:00:16 +0300

    expired jobs should not be accessible by link

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use App\Entity\Job;
 use App\Entity\Category;
@@ -32,6 +33,7 @@ class JobController extends AbstractController
     /**
      * @Route("job/{id}", name="app_job_view", requirements={"id" = "\d+"})
      * @Method("GET")
+     * @Entity("job", expr="repository.findActiveJob(id)")
      *
      * @param Job $job
      * @return Response
