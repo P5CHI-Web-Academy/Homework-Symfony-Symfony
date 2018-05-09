a7713d9936534f74d514fd22d8b8001de0180450
Constantin <kostelu@gmail.com>
2018-05-09 19:14:05 +0300

    add pagination for jobs on category view page

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,27 +2,49 @@
 
 namespace App\Controller;
 
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Category;
+use App\Repository\JobRepository;
 
-class CategoryController extends AbstractController
+class CategoryController extends Controller
 {
     /**
-     * @Route("/category/{slug}", name="app_category_view")
+     * @Route(
+     *     path="/category/{slug}/{page}",
+     *     name="app_category_view",
+     *     requirements={"page"="\d+"},
+     *     defaults={"page"=1}
+     * )
      * @Method("GET")
      *
      * @param Category $category
+     * @param int $page
+     * @param PaginatorInterface $paginator
+     * @param JobRepository $jobRepository
      * @return Response
      */
-    public function viewAction(Category $category): Response
-    {
+    public function viewAction(
+        Category $category,
+        int $page,
+        PaginatorInterface $paginator,
+        JobRepository $jobRepository
+    ): Response {
+
+        $activeJobs = $paginator->paginate(
+            $jobRepository->getActiveJobsByCategoryQuery($category),
+            $page,
+            $this->getParameter('max_jobs_on_category')
+        );
+
         return $this->render(
             'category/view.html.twig',
             [
                 'category' => $category,
+                'activeJobs' => $activeJobs,
             ]
         );
     }
--- src/Repository/JobRepository.php
+++ src/Repository/JobRepository.php
@@ -3,8 +3,11 @@
 namespace App\Repository;
 
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\AbstractQuery;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use App\Entity\Job;
+use App\Entity\Category;
 
 
 class JobRepository extends ServiceEntityRepository
@@ -22,10 +25,11 @@ class JobRepository extends ServiceEntityRepository
      */
     public function findActiveJobs()
     {
-        return $this->createQueryBuilder('j')
-            ->where('j.expiresAt > :date')
-            ->setParameter('date', new \DateTime())
-            ->orderBy('j.expiresAt', 'DESC')
+        $qb = $this->createQueryBuilder('j');
+
+        $this->applyActiveJobCondition($qb);
+
+        return $qb->orderBy('j.expiresAt', 'DESC')
             ->getQuery()
             ->getResult();
     }
@@ -37,16 +41,39 @@ class JobRepository extends ServiceEntityRepository
      */
     public function findActiveJob(int $id): ?Job
     {
-        return $this->createQueryBuilder('j')
+        $qb = $this->createQueryBuilder('j')
             ->where('j.id = :job_id')
-            ->andWhere('j.expiresAt > :expires')
-            ->setParameters(
-                [
-                    'job_id' => $id,
-                    'expires' => new \DateTime(),
-                ]
-            )
-            ->getQuery()
-            ->getOneOrNullResult();
+            ->setParameter('job_id', $id);
+
+        $this->applyActiveJobCondition($qb);
+
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+
+    /**
+     * @param Category $category
+     * @return AbstractQuery
+     */
+    public function getActiveJobsByCategoryQuery(Category $category): AbstractQuery
+    {
+        $qb = $this->createQueryBuilder('j')
+            ->where('j.category = :category')
+            ->setParameter('category', $category);
+
+        $this->applyActiveJobCondition($qb);
+
+        return $qb->orderBy('j.expiresAt', 'DESC')->getQuery();
+    }
+
+    /**
+     * @param QueryBuilder $builder
+     */
+    protected function applyActiveJobCondition(QueryBuilder $builder): void
+    {
+        $builder
+            ->andWhere('j.expiresAt > :date')
+            ->andWhere('j.activated = :activated')
+            ->setParameter('date', new \DateTime())
+            ->setParameter('activated', true);
     }
 }
